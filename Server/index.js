ff019e718682017cb81c658894989432b5db25d1
nuhla <nuhlamasri@gmail.com>
2020-02-23 21:07:27 +0200

    link home page to server main

--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const app = express();
-const PORT = (process.env.PORT = 3004);
+const PORT = (process.env.PORT = 3005);
+// const path = require('path');
+// const cors = require('cors');
 
+// app.use(cors);
 app.use(express.static('public'));
 
-//Routes
+//Using all routes for our Server
 app.use(require('./Routs/LogIn'));
 app.use(require('./Routs/Articles'));
 app.use(require('./Routs/Dashbord'));
 
-// let router = express.Router();
-// app.use(express.static('public'));
-// var routes = require('./Routs/Login');
-// app.use('/login', router);
+// app.get('/', (res, req) => {});
 
 app.listen(PORT, function() {
   console.log('Server is running at PORT:', PORT);
