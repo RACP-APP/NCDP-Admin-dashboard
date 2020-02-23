4cdad0762d00c6c52550367fdf3daf7c5cd84597
nuhla <nuhlamasri@gmail.com>
2020-02-23 22:33:41 +0200

    SignUp Compnet

--- src/Components/signUp.js
+++ src/Components/signUp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '../css/component.css';
 
 class SignUp extends React.Component {
   constructor(props) {
@@ -7,8 +7,72 @@ class SignUp extends React.Component {
   }
 
   render() {
-    return <div className="App"> this is the main app</div>;
+    return (
+      <div class="container ">
+        <div class="row">
+          <div className="Centrlize">
+            <div class="card ">
+              <div class="card-body">
+                <h5 class="card-title">SignUp to NCDP Admin dashboard</h5>
+
+                <form>
+                  <div class="form-group row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">
+                      User Name:
+                    </label>
+                    <br></br>
+                    <div class="col-sm-10">
+                      <input
+                        type="text"
+                        readonly
+                        class="form-control"
+                        id="staticEmail"
+                        placeholder="Insert Your User Name Here.."
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">
+                      Email :
+                    </label>
+                    <br></br>
+                    <div class="col-sm-10">
+                      <input
+                        type="text"
+                        readonly
+                        class="form-control"
+                        id="staticEmail"
+                        value="email@example.com"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">
+                      Password :
+                    </label>
+                    <div class="col-sm-10">
+                      <input
+                        type="password"
+                        class="form-control"
+                        id="inputPassword"
+                        placeholder="Password"
+                      />
+                    </div>
+                  </div>
+                  <button
+                    type="button"
+                    class="btn btn-primary btn-lg btn-block"
+                  >
+                    SignUp to NCDP
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default App;
+export default SignUp;
