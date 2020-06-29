3fbcccca11054e273650d0c1652af9ff157459b3
nuhla <nuhlamasri@gmail.com>
2020-06-29 17:42:44 +0300

    add progresspar to the upload

--- src/Components/Uploading/Uploading.js
+++ src/Components/Uploading/Uploading.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import '../../css/component.css';
 import '../../css/buttonStyles.css';
-import config from '../../config.json';
-import axios from 'axios';
 import io from 'socket.io';
 import socketIOClient from 'socket.io-client';
-import fs from 'fs';
+import { Progress } from 'semantic-ui-react';
 var FReader;
 var Name;
 const ENDPOINT = 'http://localhost:3000/';
 var SelectedFile;
 var socket;
+var that;
 
 class UploadingItem extends React.Component {
   constructor(props) {
@@ -26,19 +25,21 @@ class UploadingItem extends React.Component {
   }
 
   componentDidMount() {
+    that = this;
     socket = socketIOClient(ENDPOINT);
     var Path = 'http://localhost/public/uploads';
 
+    //------------------------------------------------------- Socket.io Done Event -------------------------------------------------------//
     socket.on('Done', function (data) {
       console.log('On Done Event');
       var Content = 'Video Successfully Uploaded !!';
 
       document.getElementById('UploadArea').innerHTML = Content;
+      that.setState({ uploaded: 100 });
       // document.getElementById('Restart').addEventListener('click', Refresh);
     });
-    function Refresh() {
-      // location.reload(true);
-    }
+
+    //------------------------------------------- a Function to Update the Upload % At the Dom -------------------------------------------//
     var UpdateBar = function (percent) {
       document.getElementById('ProgressBar').style.width = percent + '%';
       document.getElementById('percent').innerHTML =
@@ -46,49 +47,45 @@ class UploadingItem extends React.Component {
       var MBDone = Math.round(
         ((percent / 100.0) * SelectedFile.size) / 1048576
       );
-      document.getElementById('MB').innerHTML = MBDone;
+      that.setState({ uploaded: MBDone }, () => {
+        document.getElementById('MB').innerHTML = MBDone;
+      });
     };
 
+    //----------------------- An io.soket Event to Create Block of Date from the Large file if the file is big ---------------------------//
     socket.on('MoreData', function (data) {
       UpdateBar(data['Percent']);
       var Place = data['Place'] * 524288; //The Next Blocks Starting Position
       var NewFile = Blob; //The Variable that will hold the new Block of Data
-      if (SelectedFile.webkitSlice) {
+      if (that.state.SelectedFile.webkitSlice) {
         NewFile = SelectedFile.webkitSlice(
           Place,
-          Place + Math.min(524288, SelectedFile.size - data['Place'])
+          Place + Math.min(524288, that.state.SelectedFile.size - data['Place'])
         );
-      } else if (SelectedFile.slice) {
+      } else if (that.state.SelectedFile.slice) {
         NewFile = SelectedFile.slice(
           Place,
-          Place + Math.min(524288, SelectedFile.size - data['Place']),
+          Place +
+            Math.min(524288, that.state.SelectedFile.size - data['Place']),
           'png'
         );
-        console.log(
-          'slice',
-          'Place',
+      } else if (that.state.SelectedFile.mozSlice) {
+        NewFile = that.state.SelectedFile.mozSlice(
           Place,
-          Place + Math.min(524288, SelectedFile.size - data['Place']),
-          SelectedFile.size,
-          NewFile
-        );
-      } else if (SelectedFile.mozSlice) {
-        NewFile = SelectedFile.mozSlice(
-          Place,
-          Place + Math.min(524288, SelectedFile.size - data['Place'])
+          Place + Math.min(524288, that.state.SelectedFile.size - data['Place'])
         );
       }
+      //------------------ after creating the block of data we call the filereader stream to uplad the block ---------------------------//
       FReader.readAsBinaryString(NewFile);
     });
   }
   onClick(evnt) {
-    // const socket = socketIOClient(ENDPOINT);
     if (document.getElementById('FileBox').value != '') {
       FReader = new FileReader();
       Name = document.getElementById('NameBox').value;
       var Content =
         "<span id='NameArea'>Uploading " +
-        SelectedFile.name +
+        that.state.SelectedFile.name +
         ' as ' +
         Name +
         '</span>';
@@ -96,14 +93,14 @@ class UploadingItem extends React.Component {
         '<div id="ProgressContainer"><div id="ProgressBar"></div></div><span id="percent">0%</span>';
       Content +=
         "<span id='Uploaded'> - <span id='MB'>0</span>/" +
-        Math.round(SelectedFile.size / 1048576) +
+        Math.round(that.state.SelectedFile.size / 1048576) +
         'MB</span>';
       document.getElementById('UploadArea').innerHTML = Content;
       FReader.onload = function (evnt) {
         socket.emit('Upload', { Name: Name, Data: evnt.target.result });
       };
 
-      socket.emit('Start', { Name: Name, Size: this.state.SelectedFile.size });
+      socket.emit('Start', { Name: Name, Size: that.state.SelectedFile.size });
       // FReader.readAsArrayBuffer(this.state.file);
     } else {
       alert('Please Select A File');
@@ -147,6 +144,7 @@ class UploadingItem extends React.Component {
             Upload
           </button>
         </span>
+        <Progress percent={this.state.uploaded} autoSuccess />
       </div>
     );
   }
